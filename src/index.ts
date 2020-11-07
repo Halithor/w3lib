d0c280c8d8680c9d8ece6b1bc5826a106ba05063
Sam Marquart <marquart.sam@gmail.com>
2020-11-07 11:34:41 -0700

    export events

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './handles/index';
 export * from './hooks/index';
 export * from './system/index';
 export * from './math/index';
+export * from './events/index';
 export {tsGlobals};
 import * as tsGlobals from './globals/index';
