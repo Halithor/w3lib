417deb2847e78e6cf5be155fea6948b8bbead5c3
Sam Marquart <marquart.sam@gmail.com>
2020-11-28 14:18:03 -0700

    Event refactored; OnAnyUnitDamaged added

--- src/events/events.ts
+++ src/events/events.ts
@@ -7,13 +7,11 @@ import {vec2, Vec2} from '../math/index';
 // typed event handlers and allows their cancelation. It uses a object to
 // store the handlers, instead of an array, to facilitate easy removal of
 // registered handlers.
-class EventHandler<T extends any[]> {
+export class EventHandler<T extends any[]> {
   handlers: {[key: number]: (...input: T) => void} = {};
   count: number = 0;
 
-  constructor(readonly trg: Trigger, readonly extractor: () => T) {
-    trg.addAction(() => this.handle());
-  }
+  constructor(readonly extractor: () => T) {}
 
   handle() {
     const extracted = this.extractor();
@@ -38,14 +36,30 @@ class EventHandler<T extends any[]> {
   }
 }
 
+// teh AKA TriggerEventHandler creates an event handler and registers it for a
+// provided trigger
+function teh<T extends any[]>(trg: Trigger, extractor: () => T) {
+  const handler = new EventHandler(extractor);
+  trg.addAction(() => handler.handle());
+  return handler;
+}
+
 // Declare all the event handlers here, to be initialized in the pre-main.
 let pua: EventHandler<[u: Unit, target: Unit]>;
 let puse: EventHandler<
   [caster: Unit, abilityId: AbilId, target: Unit | Item | Destructable | Vec2]
 >;
 
+export type DamageInfo = {
+  damage: number;
+  attackType: attacktype;
+  damageType: damagetype;
+  weaponType: weapontype;
+};
+let pudamaged: EventHandler<[target: Unit, attacker: Unit, info: DamageInfo]>;
+
 addScriptHook(W3TS_HOOK.MAIN_BEFORE, () => {
-  pua = new EventHandler<[u: Unit, target: Unit]>(
+  pua = teh<[u: Unit, target: Unit]>(
     new Trigger().registerAnyUnitEvent(EVENT_PLAYER_UNIT_ATTACKED),
     () => {
       return [
@@ -54,7 +68,7 @@ addScriptHook(W3TS_HOOK.MAIN_BEFORE, () => {
       ];
     }
   );
-  puse = new EventHandler<
+  puse = teh<
     [caster: Unit, abilityId: AbilId, target: Unit | Item | Destructable | Vec2]
   >(new Trigger().registerAnyUnitEvent(EVENT_PLAYER_UNIT_SPELL_EFFECT), () => {
     const caster = Unit.fromHandle(GetSpellAbilityUnit());
@@ -74,6 +88,20 @@ addScriptHook(W3TS_HOOK.MAIN_BEFORE, () => {
     }
     return [caster, abilityId, vec2(GetSpellTargetX(), GetSpellTargetY())];
   });
+
+  pudamaged = teh<[target: Unit, attacker: Unit, info: DamageInfo]>(
+    new Trigger().registerAnyUnitEvent(EVENT_PLAYER_UNIT_DAMAGED),
+    () => {
+      const damage = GetEventDamage();
+      const target = Unit.fromHandle(BlzGetEventDamageTarget());
+      const attacker = Unit.fromHandle(GetEventDamageSource());
+      const damageType = BlzGetEventDamageType();
+      const attackType = BlzGetEventAttackType();
+      const weaponType = BlzGetEventWeaponType();
+
+      return [target, attacker, {damage, damageType, attackType, weaponType}];
+    }
+  );
 });
 
 export function onAnyUnitAttacked(
@@ -98,8 +126,29 @@ export function onAnyUnitEntersRegion(
 ) {
   // TODO cleanup after cancelation; reuse for the same region
   const trg = new Trigger().registerEnterRegion(region.handle, null);
-  const handler = new EventHandler<[entered: Unit]>(trg, () => {
+  const handler = teh<[entered: Unit]>(trg, () => {
     return [Unit.fromHandle(GetEnteringUnit())];
   });
   return handler.addHandler(callback);
+}
+
+// onAnyUnitDamage registers a callback for whenever a unit recieves damage.
+// The callback can optionally return a DamageInfo object, which will be used
+// to change the event.
+export function onAnyUnitDamaged(
+  cb: (
+    target: Unit,
+    attacker: Unit,
+    DamageInfo: DamageInfo
+  ) => DamageInfo | void
+) {
+  pudamaged.addHandler((target, attacker, info) => {
+    const updated = cb(target, attacker, info);
+    if (updated) {
+      BlzSetEventDamage(updated.damage);
+      BlzSetEventAttackType(updated.attackType);
+      BlzSetEventDamageType(updated.damageType);
+      BlzSetEventWeaponType(updated.weaponType);
+    }
+  });
 }
