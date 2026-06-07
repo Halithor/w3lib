e14c5318ddb98430e79642b460fc791bd739e77a
Jaron Hamlik <me@jaron.codes>
2026-06-07 15:46:41 -0600

    feat: add UnionSerializer

--- src/persist/serializer.test.ts
+++ src/persist/serializer.test.ts
@@ -1,5 +1,9 @@
 import { describe, expect, test } from "vitest";
-import { NumberSerializer, StringSerializer } from "./serializer";
+import {
+  NumberSerializer,
+  StringSerializer,
+  unionSerializer,
+} from "./serializer";
 
 describe("StringSerializer", () => {
   test("encode returns the value", () => {
@@ -24,5 +28,35 @@ describe("NumberSerializer", () => {
   test("decode parses a string as a number", () => {
     expect(NumberSerializer.decode("\u0004")).toBe(4);
     expect(NumberSerializer.decode("\u0000\u0000\u0000\u0004")).toBe(4);
+  });
+});
+
+describe("UnionSerializer", () => {
+  const subject = unionSerializer([NumberSerializer, StringSerializer]);
+
+  test("accepts any type in the union, rejects others", () => {
+    expect(subject.accepts("ABC")).toBe(true);
+    expect(subject.accepts(123)).toBe(true);
+
+    expect(subject.accepts(null)).toBe(false);
+    expect(subject.accepts(undefined)).toBe(false);
+    expect(subject.accepts(true)).toBe(false);
+  });
+
+  describe("encode", () => {
+    test("uses the first valid serializer", () => {
+      expect(subject.encode("ABC")).toBe("ABC");
+      expect(subject.encode(1)).toBe("\u0001");
+    });
+
+    test("throws on invalid values", () => {
+      expect(() => subject.encode(null as never)).toThrow();
+    });
+  });
+
+  describe("decode", () => {
+    test("uses the first valid serializer", () => {
+      expect(subject.decode("\u0004")).toBe(4);
+    });
   });
 });
--- src/persist/serializer.ts
+++ src/persist/serializer.ts
@@ -1,11 +1,16 @@
 import { asNumber, asString, minWidth } from "./helper";
 
 export interface Serializer<T> {
+  accepts(value: unknown): value is T;
   encode(value: T): string;
   decode(value: string): T;
 }
 
 export const StringSerializer: Serializer<string> = {
+  accepts(value: unknown) {
+    return typeof value === "string";
+  },
+
   encode(value: string): string {
     return value;
   },
@@ -16,6 +21,10 @@ export const StringSerializer: Serializer<string> = {
 };
 
 export const NumberSerializer: Serializer<number> = {
+  accepts(value: unknown) {
+    return typeof value === "number";
+  },
+
   encode(value: number): string {
     return asString(value, minWidth(value));
   },
@@ -24,3 +33,48 @@ export const NumberSerializer: Serializer<number> = {
     return asNumber(value);
   },
 };
+
+class UnionSerializer<T> implements Serializer<T> {
+  private serializers: readonly Serializer<unknown>[];
+
+  constructor(serializers: readonly Serializer<unknown>[]) {
+    this.serializers = serializers;
+  }
+
+  accepts(value: unknown): value is T {
+    return this.serializers.some((serializer) => serializer.accepts(value));
+  }
+
+  encode(value: T): string {
+    for (const serializer of this.serializers) {
+      if (serializer.accepts(value)) {
+        return serializer.encode(value);
+      }
+    }
+
+    throw new Error("TODO");
+  }
+
+  decode(value: string): T {
+    for (const serializer of this.serializers) {
+      try {
+        return serializer.decode(value) as T;
+      } catch {
+        // Do nothing. Try the next serializer.
+      }
+    }
+
+    throw new Error("TODO");
+  }
+}
+
+export type { UnionSerializer };
+
+type Serializers<A extends readonly Serializer<unknown>[]> =
+  A[number] extends Serializer<infer U> ? U : never;
+
+export function unionSerializer<const A extends readonly Serializer<unknown>[]>(
+  serializers: A,
+): UnionSerializer<Serializers<A>> {
+  return new UnionSerializer(serializers);
+}
