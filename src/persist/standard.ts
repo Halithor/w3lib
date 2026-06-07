0b9a744c30835ca7ac39aac3be0cab6e2ae6fa38
Jaron Hamlik <me@jaron.codes>
2026-06-07 14:56:31 -0600

    cleanup: add BaseKey<T>

--- src/persist/standard.ts
+++ src/persist/standard.ts
@@ -1,13 +1,18 @@
 import { Key } from "./table";
 import { asNumber, asString, minWidth } from "./helper";
 
-export class StringKey implements Key<string> {
+export abstract class BaseKey<T> implements Key<T> {
   id: number;
 
   constructor(id: number) {
     this.id = id;
   }
 
+  abstract encode(value: T): string;
+  abstract decode(value: string): T;
+}
+
+export class StringKey extends BaseKey<string> {
   encode(value: string): string {
     return value;
   }
@@ -17,13 +22,7 @@ export class StringKey implements Key<string> {
   }
 }
 
-export class NumberKey implements Key<number> {
-  id: number;
-
-  constructor(id: number) {
-    this.id = id;
-  }
-
+export class NumberKey extends BaseKey<number> {
   encode(value: number): string {
     return asString(value, minWidth(value));
   }
